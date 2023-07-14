9085685e262ad89cf36f9b370d857fb512e3906f
muco0521 <muco0521@gmail.com>
2023-07-14 21:26:33 +0800

    feat: add isAuthenticated to display different view templates

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-  return res.send('expense-tracker project.')
+  return res.render('index')
 })
 
 module.exports = router
