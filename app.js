0f73577e16188ce7fe396204520fb516ad4374a1
muco0521 <muco0521@gmail.com>
2023-07-14 13:26:08 +0800

    feat: add login and register page & routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const routes = require('./routes/index')
 const app = express()
 const PORT = 3000
 
@@ -9,9 +10,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('expense-tracker project')
-})
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
