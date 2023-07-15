05935a615e75bd621cd074e7e9e224f7b7586a3b
muco0521 <muco0521@gmail.com>
2023-07-16 06:10:13 +0800

    feat: add home page

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,11 +1,11 @@
 const db = require('../../config/mongoose')
 const Category = require('../category')
 const SEED_CATEGORY = {
-  家居物業: '<i class="fa-solid fa-house"></i>',
-  交通出行: '<i class="fa-solid fa-van-shuttle"></i>',
-  休閒娛樂: '<i class="fa-solid fa-face-grin-beam"></i>',
-  餐飲食品: '<i class="fa-solid fa-utensils"></i>',
-  其他: '<i class="fa-solid fa-pen"></i>',
+  家居物業: 'fa-house',
+  交通出行: 'fa-van-shuttle',
+  休閒娛樂: 'fa-face-grin-beam',
+  餐飲食品: 'fa-utensils',
+  其他: 'fa-pen',
 }
 
 const categories = []
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,25 @@
 const router = require('express').Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
-  return res.render('index')
+  const userId = req.user._id
+  Category.find({})
+    .lean()
+    .then((categories) => {
+      Record.find({ userId })
+        .populate('categoryId')
+        .lean()
+        .then((records) => {
+          let totalAmount = 0
+          records.map((record) => {
+            totalAmount += record.amount
+            record.date = new Date(record.date).toLocaleString().slice(0, 8)
+          })
+          res.render('index', { categories, records, totalAmount })
+        })
+        .catch((e) => console.log(e))
+    })
 })
 
 module.exports = router
